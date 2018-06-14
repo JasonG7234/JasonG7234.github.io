4b751c8215c72817538beeb8d7cd0017b9879d21
Jason Gomes <JasonG7234@gmail.com>
2018-06-13 23:30:59 -0400

    Accordion JS and formatting update

---
+++ nba_draft/accordion1.js
@@ -0,0 +1,28 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    var panel = event.target.parentElement.parentElement;  panel.querySelector("." + tabName).style.display = "block";
+    evt.target.className += " active";
+}
