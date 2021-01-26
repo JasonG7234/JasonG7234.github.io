d8c98e83fba5a096d05115d3c1090a9a9336d52d
JasonG7234 <JasonG7234@gmail.com>
2021-01-26 17:41:36 -0500

    Playing around with about.html page

--- nba_draft/accordion.js
+++ nba_draft/accordion.js
@@ -1,8 +1,28 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+var acc = document.getElementsByClassName("accordion"), i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
+        var oldAcc = undefined;
+        for (i = 0; i < acc.length; i++) {
+            if(acc[i].className.length > 9) {
+                oldAcc = acc[i];
+                break;
+            }
+        }
+        if(oldAcc && oldAcc != this) {
+            var oldPanel = oldAcc.nextElementSibling;
+            var oldTabContent = oldPanel.querySelectorAll(".tabcontent");
+            for(i = 0; i < oldTabContent.length; i++) {
+                if(oldTabContent[i].style.display === "block") {
+                    var oldTablinks = oldPanel.querySelector(".tab").querySelectorAll(".tablinks");
+                    oldTabContent[i].style.display = "none";
+                    oldTablinks[i].classList.toggle("active");
+                    break;
+                };
+            }
+            oldPanel.style.display = "none";
+            oldAcc.className = "accordion";
+        }
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
@@ -20,9 +40,19 @@ function openTab(evt, tabName) {
         tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
+    var oldTab = undefined;
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        if(tablinks[i].className.length > 8) {
+            oldTab = tablinks[i];
+            oldTab.classList.toggle("active");
+            break;
+        }
     }
-    var panel = event.target.parentElement.parentElement;  panel.querySelector("." + tabName).style.display = "block";
-    evt.target.className += " active";
+    var panel = evt.target.parentElement.parentElement;  
+    if (oldTab === evt.target) {
+        panel.querySelector(`.${tabName}`).style.display = "none";
+    } else {
+        panel.querySelector(`.${tabName}`).style.display = "block";
+        evt.target.classList.toggle("active");
+    }
 }
